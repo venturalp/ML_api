664ee7c90ad0b627f3a793f227b77bf5c74d45ac
Guilherme Ventura <venturalp@gmail.com>
2017-12-01 01:48:19 -0200

    configurando node, menu com lista dinamica e variaveis

--- gulpfile.js
+++ gulpfile.js
@@ -4,37 +4,55 @@ var rename = require('gulp-rename');
 var reactEasy = require('gulp-react-easy');
 var rupture = require('rupture');
 var stylus = require('gulp-stylus');
+var nodemon = require('gulp-nodemon');
 
 //Seta tasks que serão chamadas no início da execução do gulp
-gulp.task('default', ['js', 'w', 'comp-stylus']);
+gulp.task('default', ['js', 'start-server', 'w', 'comp-stylus']);
+
+gulp.task('start-server', function () {
+	var stream = nodemon({
+		script: 'server.js',
+		ext: 'js',
+		ignore:['site']
+	})
+
+	stream
+		.on('restart', function () {
+			console.log('restarted!')
+		})
+		.on('crash', function () {
+			console.error('Application has crashed!\n')
+			stream.emit('restart', 10) // restart the server in 10 seconds 
+		})
+});
 
 //Adiciona tarefa de watcher
 gulp.task('w', function () {
 	//assite alterações no diretório /src/js e subdiretórios e se houver alguma alteração executa a task 'js'
-	gulp.watch('./src/js/**/*', ['js']);
-	gulp.watch('./stylus/**/*.styl', ['comp-stylus'])
+	gulp.watch('./site/src/js/**/*', ['js']);
+	gulp.watch('./site/stylus/**/*.styl', ['comp-stylus'])
 });
 
 //Task de compilação de stylus
-gulp.task('comp-stylus', function(){
+gulp.task('comp-stylus', function () {
 	var options = {
 		//plugins usados com o stylus
-        use: [ rupture() ]
-    };
-	
-	gulp.src('./stylus/general.styl')//arquivo principal do stylus
-        .pipe(stylus(options))
-        .pipe(rename('general.css'))//nome do arquivo compilado
-        .pipe(gulp.dest('./dist/css/')); //destino do arquivo compilado
+		use: [rupture()]
+	};
+
+	gulp.src('./site/stylus/general.styl') //arquivo principal do stylus
+		.pipe(stylus(options))
+		.pipe(rename('general.css')) //nome do arquivo compilado
+		.pipe(gulp.dest('./site/dist/css/')); //destino do arquivo compilado
 });
 
 //task 'js'
 gulp.task('js', function () {
 	//gera a compilação do fonte em react
 	return reactEasy({
-			file: './src/js/app.jsx', //arquivo jsx a ser compilado/gerado
+			file: './site/src/js/app.jsx', //arquivo jsx a ser compilado/gerado
 			debug: true // optional, false by default
 		})
 		.to('app.js')
-		.pipe(gulp.dest('./dist/js/'));
+		.pipe(gulp.dest('./site/dist/js/'));
 });
--- src/js/titulo/titulo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Titulo extends React.Component {
-	render(txt){
-		return (<h1>Curso de React!</h1>);
-	};
-}
-
-export default Titulo;
